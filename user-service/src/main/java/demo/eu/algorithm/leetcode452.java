d647f11a21cb2d97ddf1678ceb77def282f7c9d1
Circle <291960800@qq.com>
2025-04-05 20:13:41 +0800

    111

---
+++ user-service/src/main/java/demo/eu/algorithm/leetcode452.java
@@ -0,0 +1,28 @@
+package demo.eu.algorithm;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class leetcode452 {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        System.out.println(Arrays.deepToString(points));
+        int n = points.length;
+        int cnt = 1;
+        int end = points[0][1];
+        for (int i = 1; i < n; i++) {
+            if (points[i][0] > end){
+                cnt++;
+                end = points[i][1];
+            }
+            else{
+                end = Math.min(end, points[i][1]);
+            }
+        }
+        return cnt;
+    }
+}
---
+++ user-service/src/main/java/demo/eu/algorithm/leetcode55.java
@@ -0,0 +1,15 @@
+package demo.eu.algorithm;
+
+public class leetcode55 {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int cover = 0;
+        for(int i = 0; i <= cover; i++){
+            cover = Math.max(cover, nums[i]+i);
+            if(cover >= n-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
