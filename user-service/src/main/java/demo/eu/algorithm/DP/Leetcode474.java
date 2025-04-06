ed37fda7dbf8ece9f691d062bd049b572bfbec16
Circle <291960800@qq.com>
2025-04-06 17:37:18 +0800

    111

--- user-service/src/main/java/demo/eu/algorithm/DP/Leetcode474.java
+++ user-service/src/main/java/demo/eu/algorithm/DP/Leetcode474.java
@@ -1,6 +1,9 @@
 package demo.eu.algorithm.DP;
 
 import java.util.Arrays;
+//给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+//请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+//如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
 
 public class Leetcode474 {
     public int findMaxForm(String[] strs, int m, int n) {
