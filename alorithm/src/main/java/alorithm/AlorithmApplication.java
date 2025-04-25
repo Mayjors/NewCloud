a8afd602afc0a07352b0e0e7e50016774a0d3605
mayjors <983534678@qq.com>
2025-04-25 20:16:06 +0800

    新增alorithm模块

---
+++ alorithm/src/main/java/alorithm/AlorithmApplication.java
@@ -0,0 +1,13 @@
+package alorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlorithmApplication.class, args);
+    }
+
+}
---
+++ alorithm/src/test/java/alorithm/AlorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package alorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
